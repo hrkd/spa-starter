1820d0fa130b8310b4ff36b052eed6a004788e79
hrkd <hiroki@hrkd.net>
2021-03-30 12:17:18 +0900

    update: api client

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
 import { fetch, Endpoint } from '../utils/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleLoading } from '../redux/index';
 
 export const useApi = (endpoint: Endpoint) => {
   const [url] = useState<Endpoint>(endpoint);
   const [data, setData] = useState<number>(0);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const doFetch = async () => {
-      const result = await fetch(url);
-      setData(result.data);
+      dispatch(toggleLoading(true));
+      try {
+        const result = await fetch(url);
+        setData(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+      dispatch(toggleLoading(false));
     };
     doFetch();
   }, [url]);
